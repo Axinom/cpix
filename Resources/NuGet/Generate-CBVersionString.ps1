26f20899d4d1242aa507a8a3d9d74ff9bb4ef596
Sander Saares <saares@axinom.com>
2016-11-10 09:25:51 +0200

    Added Continous Build verison number script

---
+++ Resources/NuGet/Generate-CBVersionString.ps1
@@ -0,0 +1,23 @@
+﻿$ErrorActionPreference = "Stop"
+
+$ns = @{
+	spec = "http://schemas.microsoft.com/packaging/2010/07/nuspec.xsd"
+}
+
+$versionNode = Select-Xml -Xpath "/spec:package/spec:metadata/spec:version" -Path "Cpix.nuspec" -Namespace $ns
+$version = $versionNode.Node.InnerText
+
+Write-Host "NuSpec version is $version"
+
+# If there is already some -pre01 or whatever suffix, get rid of it.
+$version = ($version -split "-")[0]
+
+Write-Host "Cleaned version is $version"
+
+$suffix = "-cb" + $(Get-Date -Format "FileDateTimeUniversal")
+
+$version = $version + $suffix
+
+Write-Host "CB version is $version"
+
+Write-Host "##vso[task.setvariable variable=CbNugetPackageVersion;]$version"
